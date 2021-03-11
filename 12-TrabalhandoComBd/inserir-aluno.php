84e87202aa9be9516c9887ccff5dde51080b7121
gustavobrod <gustavo.brod@outlook.com>
2021-03-11 20:32:10 -0300

    PHPePDO_Mod2_Aula4

--- 12-TrabalhandoComBd/inserir-aluno.php
+++ 12-TrabalhandoComBd/inserir-aluno.php
@@ -9,7 +9,8 @@ $pdo = new PDO('sqlite: . $databasePath');
 
 $student = new Student(null, 'Gustavo Brod', new \DateTimeImmutable('1981-05-25'));
 
-$sqlInsert = "INSERT INTO students (name, birth_date) VALUE ('{$student->name()}', '{$student->birthDate()->format('Y-m-d')}')";
+$sqlInsert = "INSERT INTO students (name, birth_date) VALUES ('{$student->name()}', '{$student->birthDate()->format('Y-m-d')}')";
 
-var_dump($pdo->exec($sqlInsert)) . PHP_EOL;
+var_dump ($pdo->exec($sqlInsert)) . PHP_EOL;
 
+ 

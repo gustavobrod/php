a20f6b59d98ce3913ba5c4b01b4703a7b96664de
gustavobrod <gustavo.brod@outlook.com>
2021-03-26 00:23:10 -0300

    PHPePDO_Mod7_Aula2

--- 12-TrabalhandoComBd/src/Domain/Model/Student.php
+++ 12-TrabalhandoComBd/src/Domain/Model/Student.php
@@ -7,6 +7,8 @@ class Student
     private ?int $id;
     private string $name;
     private \DateTimeInterface $birthDate;
+    /**@var Phone[] */
+    private array $phone = [];
 
     public function __construct(?int $id, string $name, \DateTimeInterface $birthDate)
     {
@@ -49,5 +51,16 @@ class Student
         return $this->birthDate
             ->diff(new \DateTimeImmutable())
             ->y;
+    }
+
+    public function addPhone(Phone $phone): void
+    {
+        $this->phones[] = $phone;
+    }
+
+    /** @return Phonenn[] */
+    public function phones(): array
+    {
+        return $this->phones;
     }
 }

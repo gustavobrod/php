5629c0c933acf1507a833993382b6c5f0a4ab2fb
gustavobrod <gustavo.brod@outlook.com>
2021-03-25 21:38:05 -0300

    PHPePDO_Mod5_Aula4

--- 12-TrabalhandoComBd/criar-turma.php
+++ 12-TrabalhandoComBd/criar-turma.php
@@ -12,18 +12,18 @@ $studentRepository = new PdoStudentRepository($connection);
 $connection->beginTransaction();
 $aStudent = new Student(
     null,
-    'Nico Stppat',
-    new DateTimeImmutable('1963-07-31')
+    'Adriana Souza',
+    new DateTimeImmutable('1999-01-31')
 );
 
 $studentRepository->save($aStudent);
 
 $anotherStudent = new Student(
     null,
-    'Alline Silva',
-    new DateTimeImmutable('1983-05-20')
+    'João Antonio',
+    new DateTimeImmutable('2000-12-21')
 );
 
 $studentRepository->save($anotherStudent);
 
-$connection->commit();
+$connection->rollBack();

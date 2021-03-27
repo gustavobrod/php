7107727a3025706c20f726518076881d5fc19818
gustavobrod <gustavo.brod@outlook.com>
2021-03-26 21:46:05 -0300

    Doct2_aula5

---
+++ 13-DoctrineAlura/cli-config.php
@@ -0,0 +1,11 @@
+<?php
+
+use Alura\Doctrine\Helper\EntityManagerFactory;
+use Doctrine\ORM\Tools\Console\ConsoleRunner;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$entityManager = new EntityManagerFactory();
+$entityManager = $entityManager->getEntityManager();
+
+return ConsoleRunner::createHelperSet($entityManager);

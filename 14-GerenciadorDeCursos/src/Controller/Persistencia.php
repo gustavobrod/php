cfcc83dba8d08a8dc418bc74104d0233f0e18bf8
gustavobrod <gustavo.brod@outlook.com>
2021-04-28 00:02:37 -0300

    ListCurso3aula4

--- 14-GerenciadorDeCursos/src/Controller/Persistencia.php
+++ 14-GerenciadorDeCursos/src/Controller/Persistencia.php
@@ -19,6 +19,7 @@ class Persistencia implements InterfaceControladorRequiscao
 
     public function processaRequisicao(): void
     {
+        $descricao = filter_input(INPUT_POST, 'descricao', FILTER_SANITIZE_STRING);
         $curso = new Curso();
         $curso->setDescricao($_POST['descricao']);
         $this->entityManager->persist($curso);

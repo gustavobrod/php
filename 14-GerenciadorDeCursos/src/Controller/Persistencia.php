534ab7973dd7e99ad24a372751474b05b122ba51
gustavobrod <gustavo.brod@outlook.com>
2021-04-27 23:46:36 -0300

    ListCurso3aula2

---
+++ 14-GerenciadorDeCursos/src/Controller/Persistencia.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Alura\Cursos\Controller;
+
+use Alura\Cursos\Entity\Curso;
+use Alura\Cursos\Infra\EntityManagerCreator;
+
+class Persistencia implements InterfaceControladorRequiscao
+{
+    /**
+     * @var \Doctrine\ORM\EntityManagerInterface
+     */
+    private $entityManager;
+
+    public function __construct()
+    {
+        $this->entityManager = (new EntityManagerCreator())->getEntityManager();
+    }
+
+    public function processaRequisicao(): void
+    {
+        $curso = new Curso();
+        $curso->setDescricao($_POST['descricao']);
+        $this->entityManager->persist($curso);
+        $this->entityManager->flush();
+    }
+}

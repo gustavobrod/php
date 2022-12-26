0b4757cbb1421de878b1ddc2cc623db478d3c92a
Gustavo Brod <gustavo.brod@outlook.com>
2022-12-26 20:56:45 -0300

    26.12.2022

---
+++ curso_udemy/PHP_do_zero_a_maestria/01_intro/nome.php
@@ -0,0 +1,8 @@
+<?php
+
+echo "Gustavo Lima Brod";
+
+"Soma"(7+2);
+print (Soma);
+
+?>
---
+++ curso_udemy/PHP_do_zero_a_maestria/02_conceitos_basicos/1_case_sensitive/index.php
@@ -0,0 +1,15 @@
+<?php
+
+// Nâo é case sensitive
+echo "teste <br>";
+eChO "testando 2 <br>";
+ECHO "testando 4 <br>";
+
+//É case sensitive
+$nome = "Gustavo";
+$NOME = "GUSTAVO";
+
+echo $nome;
+echo "<br>";
+echo $NOME;
+

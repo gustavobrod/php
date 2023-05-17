703870ae4592beffa157d6b9973d0874be21d6ad
Gustavo Brod <gustavo.brod@outlook.com>
2023-05-17 00:15:46 -0300

    cursoEmVideo_17-05-2023

---
+++ CursoEmVideo/CursoPHP-Iniciantes/aula01.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <title> Primeiro Exemplo PHP</title>
+        <style>
+            h2 {
+                color: #FF69B4;
+                text-shadow: 1px 1px 1px black;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>Testando PHP</h1>
+        <?php
+            echo "<h2> Ola, <br/> Mundo! </h2>";
+        ?>
+        </body>
+</html>
